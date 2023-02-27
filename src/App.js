be85c492b6ea452907f9df0e9e1cad6d7fda3f62
steveshammah <shammahodhiambo@gmail.com>
2023-02-27 12:15:54 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { RedocStandalone } from "redoc";
 
 function App() {
   return (
@@ -18,6 +19,31 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <RedocStandalone
+        specUrl="/openapi.yaml"
+        options={{
+          nativeScrollbars: true,
+          theme: {
+            colors: { primary: { main: "#66429C" } },
+            typography: { headings: { fontWeight: "600" } },
+            sidebar: {
+              backgroundColor: "#fff",
+              groupItems: { activeBackgroundColor: "#66429c33" },
+              level1Items: {
+                activeBackgroundColor: "#66429c33",
+                activeTextColor: "#66429C",
+              },
+            },
+            rightPanel: {
+              backgroundColor: "#0A2540",
+            },
+          },
+          scrollYOffset: 70,
+          hideDownloadButton: true,
+          // hideLoading: true,
+        }}
+      />
     </div>
   );
 }
